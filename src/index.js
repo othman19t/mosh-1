29be268975a3e119714a91bfd75438fb56f9a5db
othman19t <othman19t@gmail.com>
2020-06-21 20:48:52 -0700

    using immutable library to work with objects

--- src/index.js
+++ src/index.js
@@ -1,4 +1,5 @@
 import { compose, pipe } from "lodash/fp"; //npm i lodash
+import { Map } from "immutable";
 let input = "       text      ";
 
 // reuseable function
@@ -14,3 +15,8 @@ console.log(result);
 const transform2 = pipe(trim, lower); //applies from left to right
 const result2 = transform(input);
 console.log(result2);
+
+let book = Map({ title: "this is the title" }); // warp the object with Map which comes from immutable library.
+book = book.set("title", "this is the updated title"); // update or add new property to an object in immutable library.
+
+console.log(book.get("title")); // get a property using immutable library.
