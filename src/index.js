9f469848d8b2ef7df5daf34dce0650f71d4df4d2
othman19t <othman19t@gmail.com>
2020-06-21 19:32:13 -0700

    adding lodash commants

--- src/index.js
+++ src/index.js
@@ -6,11 +6,11 @@ const trim = (str) => str.trim();
 const lower = (str) => str.toLowerCase();
 
 //using compose to organize your code instead of wrapping functions
-const transform = compose(lower, trim);
+const transform = compose(lower, trim); //applies from right to left
 const result = transform(input);
 console.log(result);
 
 //using pipe to organize your code instead of wrapping functions
-const transform2 = pipe(trim, lower);
+const transform2 = pipe(trim, lower); //applies from left to right
 const result2 = transform(input);
 console.log(result2);
